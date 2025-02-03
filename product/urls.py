e230740bdb3545f4f16ac55fd03950f1403e2451
LHJ0530 <dlgml0105@naver.com>
2025-02-03 23:59:08 +0900

    3주차 커밋

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from.import views
+app_name = 'product'
 
 urlpatterns = [
     path('', views.mainpage),
+    path('loginsign/', views.loginsign, name='loginsign'),
+    path('chatting/', views.chatting, name='chatting'),
+    path('sign/', views.sign, name='sign'),
+    path('todaynews/', views.todaynews, name='todaynews'),
+    path('likecheck/', views.likecheck, name='likecheck'),
+    
    
 ]
--- product/views.py
+++ product/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 
 
-def mainpage(reqeust):
-    return render(reqeust,'product/mainpage.html')
+def mainpage(request):
+    return render(request,'product/mainpage.html')
+
+
+def loginsign(request):
+    return render(request, 'product/loginsign.html' )
+
+def chatting(request):
+    return render(request, 'product/chatting.html' )
+
+def likecheck(request):
+    return render(request, 'product/likecheck.html' )
+
+def sign(request):
+    return render(request, 'product/sign.html' )
+
+def todaynews(request):
+    return render(request, 'product/todaynews.html' )
+
 
 # Create your views here.
